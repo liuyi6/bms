cd6c0573fd226c0f20b281d26bb072ee08a9a5f8
liuyi6 <ly616mail@163.com>
2019-05-18 13:09:40 +0800

    运行环境搭建

--- bms-web/src/main/java/com/luis/bms/Main.java
+++ bms-web/src/main/java/com/luis/bms/Main.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import tk.mybatis.spring.annotation.MapperScan;
 
 @SpringBootApplication
-@MapperScan("com.luis.bms.system.dao")
+@MapperScan("com.luis.bms.system.mapper")
 public class Main {
 	public static void main(String[] args) {
 		SpringApplication.run(Main.class, args);
